fa84d6045ecf7da634c8f62dd404154e409c4e55
kishorevins <kishore.s@vinsinfo.com>
2026-03-26 15:24:09 +0530

    intial comit

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   // Change this to match your GitHub repository name exactly
-  base: '/cmrl/',
+  base: '/vins-cmrl/',
   build: {
     rollupOptions: {
       output: {
